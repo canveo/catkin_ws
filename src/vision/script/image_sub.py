1d41905ad92a58381f87a7301f7dc77b1af40793
canveo <canveo@gmail.com>
2021-01-27 11:59:29 -0300

    publisher and subscriber created

--- src/vision/script/image_sub.py
+++ src/vision/script/image_sub.py
@@ -1,14 +1,95 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 
 import rospy
 import cv2
+import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from cv_bridge import  CvBridge, CvBridgeError
+from cv_bridge import CvBridge, CvBridgeError
 import sys
 import numpy as np
 
-from vision.script.video_utils import ball_detected
+
+
+def filter_color(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # cv2.imshow("hsv image", hsv)
+
+    #find the upper and lower bounds the yellow color
+    yellowLower = (30, 150, 100)
+    yellowUpper = (50, 255, 255)
+
+    #define a mask using the lower andthe upper bounds of the yellow color
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+
+    cv2.imshow("mask image", mask)
+    return mask
+
+def getContours(binary_image):      
+    contours, hierarchy = cv2.findContours(binary_image, 
+                                              cv2.RETR_TREE, 
+                                               cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+def draw_contours(image, contours, image_name):
+    index = -1 
+    thickness = 2
+    color = (0, 0, 255) 
+    cv2.drawContours(image, contours, index, color, thickness)
+    cv2.imshow(image_name,image)
+
+def convert_gray_to_binary(gray_image):
+    binary_image = cv2.adaptiveThreshold(gray_image, 
+                            255, 
+                            cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                            cv2.THRESH_BINARY_INV, 115, 2)
+    
+    return binary_image
+
+def draw_ball_contour(binary_image, rgb_image, contours):
+    black_image = np.zeros([binary_image.shape[0], binary_image.shape[1],3],'uint8')
+    
+    for c in contours:
+        area = cv2.contourArea(c)
+        perimeter= cv2.arcLength(c, True)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        if (area>3000):
+            cv2.drawContours(rgb_image, [c], -1, (150,250,150), 1)
+            cv2.drawContours(black_image, [c], -1, (150,250,150), 1)
+            cx, cy = get_contour_center(c)
+            cv2.circle(rgb_image, (cx,cy),(int)(radius),(0,0,255),1)
+            cv2.circle(black_image, (cx,cy),(int)(radius),(0,0,255),1)
+            cv2.circle(black_image, (cx,cy),5,(150,150,255),-1)
+            # print ("Area: {}, Perimeter: {}".format(area, perimeter))
+    # print ("number of contours: {}".format(len(contours)))
+    cv2.imshow("RGB Image Contours",rgb_image)
+    cv2.imshow("Black Image Contours",black_image)
+
+def get_contour_center(contour):
+    M = cv2.moments(contour)
+    cx=-1
+    cy=-1
+    if (M['m00']!=0):
+        cx= int(M['m10']/M['m00'])
+        cy= int(M['m01']/M['m00'])
+    return cx, cy
+
+
+def ball_detected(frame):
+    mask = filter_color(frame)
+    contours = getContours(mask)
+    draw_ball_contour(mask, frame, contours)
+    cv2.imshow('Mask', mask)
+    
+
+def main(args):
+    i_sub = image_subscriber()
+    rospy.init_node("Image_Subscriber", anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shuting down")
+    cv2.destroyAllWindows()
 
 
 class image_subscriber:
@@ -16,84 +97,19 @@ class image_subscriber:
         self.image_sub = rospy.Subscriber("image_topic", Image, self.callback)
         self.bridge = CvBridge()
 
-    def color_filter(frame):
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # cv2.imshow("hsv image", hsv)
-
-        #find the upper and lower bounds the yellow color
-        yellowLower = (30, 150, 100)
-        yellowUpper = (50, 255, 255)
-
-        #define a mask using the lower andthe upper bounds of the yellow color
-        mask = cv2.inRange(hsv, yellowLower, yellowUpper)
-
-        cv2.imshow("mask image", mask)
-
-        return mask
-
-    def getContours(binary_image):      
-        contours, hierarchy = cv2.findContours(binary_image, 
-                                              cv2.RETR_TREE, 
-                                               cv2.CHAIN_APPROX_SIMPLE)
-        return contours
-
-
     def callback(self, data):
-        # try:
-        #     # cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        #     cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        #     # self.hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        #     # cv2.imshow("Image window", hsv)
-        #     # print(self.cv_image)
-        #     # Convert the image to a Numpy array since most cv2 functions
-        # # require Numpy arrays
-        #     # cv_image = np.array(cv_image, dtype=np.uint8)
-                        
-        #     # cv2.imshow("Image window", cv_image)
-
-        # except CvBridgeError as e:
-        #     print(e)
-        
-        # # Convert the image to a Numpy array since most cv2 functions
-        # # require Numpy arrays
-        # cv_image = np.array(cv_image, dtype=np.uint8)
-        # # mask = self.filter_color(self.cv_image)
-        # print(cv_image)
-        # cv2.imshow("Image window", cv_image)
-        # while not rospy.is_shutdown():
-        #     cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        #     cv2.imshow("Imagen subs", cv_image)
-        #     print("hola")
-        #     cv2.waitKey(0)
-
-        #     if cv2.waitKey(25) & 0xFF == ord('q'):
-        #         break
         try:
-            i_sub = image_subscriber()
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            # cv_image = np.array(cv_image, dtype=np.uint8)
-            # mask = i_sub.color_filter(cv_image)
-            # contours = i_sub.getContours(mask)
-            # cv2.imshow("Imagen subs", contours)
-            ball_detected(cv_image)
+            frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            mask = filter_color(frame)
+            contours = getContours(mask)
+            draw_ball_contour(mask, frame, contours)
+            cv2.imshow("Image2", mask)
             cv2.waitKey(1)
+
         except CvBridgeError as e:
             print(e)
 
- 
-
-
-  
-def main(args):
-    i_sub = image_subscriber()
-    rospy.init_node("Image_Subscriber", anonymous=True)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
+if __name__== "__main__":
     main(sys.argv)
-    
+
+
---
+++ src/vision/script/image_sub_old.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import  CvBridge, CvBridgeError
+import sys
+import numpy as np
+
+# from vision.script.video_utils import ball_detected
+
+
+class image_subscriber:
+    def __init__(self):
+        self.image_sub = rospy.Subscriber("image_topic", Image, self.callback)
+        self.bridge = CvBridge()
+
+    def color_filter(frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # cv2.imshow("hsv image", hsv)
+
+        #find the upper and lower bounds the yellow color
+        yellowLower = (30, 150, 100)
+        yellowUpper = (50, 255, 255)
+
+        #define a mask using the lower andthe upper bounds of the yellow color
+        mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+
+        cv2.imshow("mask image", mask)
+
+        return mask
+
+    def getContours(binary_image):      
+        contours, hierarchy = cv2.findContours(binary_image, 
+                                              cv2.RETR_TREE, 
+                                               cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+
+    def callback(self, data):
+        try:
+            i_sub = image_subscriber()
+            frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            # cv_image = np.array(cv_image, dtype=np.uint8)
+            mask = i_sub.color_filter(frame)
+            # contours = i_sub.getContours(mask)
+            # cv2.imshow("Imagen subs", mask)
+            # ball_detected(cv_image)
+            print(cv_image)
+            cv2.waitKey(1)
+        except CvBridgeError as e:
+            print(e)
+
+ 
+def main(args):
+    i_sub = image_subscriber()
+    rospy.init_node("Image_Subscriber", anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
