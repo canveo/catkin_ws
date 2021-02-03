3c931df576529ad7e1e48dc0e69ba131914d18dc
canveo <canveo@gmail.com>
2021-02-03 20:00:45 -0300

    control proporcional funcionando

--- src/vision/script/laserscan_P_action.py
+++ src/vision/script/laserscan_P_action.py
@@ -7,17 +7,17 @@ import math
 import time
 from simple_pid import PID
 
+
 # sensor data
 front = 0.5
 left = 0.5
 rigth = 0.5
 
 # pid data
+linear_pid = PID(0.5, 0, 0, output_limits=(-1.0, 1.0), auto_mode=True)
+angular_pid = PID(0.5, 0, 0, output_limits=(-1.0, 1.0))
 
-linear_pid = PID(kp=0.5, ki=0, kd=0, output_limits=(-1, 1))
-angular_pid = PID(kp=0.5, ki=0, kd=0, output_limits=(-1, 1))
-
-linear_pid.setpoint = 0.6
+linear_pid.setpoint = 0.8
 angular_pid.setpoint = 0
 
 linear_pid.sample_time = 0.01
@@ -27,9 +27,15 @@ angular_pid.sample_time = 0.01
 def scan_callback(scan_data):
     global front, left, rigth
     # min_distance = min(scan_data.ranges[358:360])
-    front = scan_data.ranges[360]
+    front = scan_data.ranges[359]
+    if front == math.inf:
+        front = 2
     left = scan_data.ranges[90]
+    if left == math.inf:
+        left = 2.0
     rigth = scan_data.ranges[270]
+    if rigth == math.inf:
+        rigth = 2.0
     print("f:{}  l:{}  r:{}".format(front, left, rigth))
 
 def move_and_rotate(avoid_obstacle):
@@ -40,19 +46,15 @@ def move_and_rotate(avoid_obstacle):
     loop_rate =rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        cmd_vel.linear.x = linear_pid(front)          # output linear
+        cmd_vel.linear.x = -linear_pid(front)          # output linear
         cmd_vel.angular.z = angular_pid(left-rigth)   # output angular
 
         velocity_publisher.publish(cmd_vel)
-        # print("cmd = {}, min dis = {}".format(cmd_vel.angular.z, min_distance))
+        print("linear out= {}, angular out = {}".format(cmd_vel.linear.x, cmd_vel.angular.z))
 
     cmd_vel.linear.x = 0.0
     cmd_vel.angular.z = 0.0
     velocity_publisher(cmd_vel)
-
-def calc_error():
-    global front, left, rigth
-    error_c = (left - rigth)
 
             
 if __name__ == '__main__':
